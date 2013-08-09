31413933d1e324dd4358cdcb53fcfe8b7a95bd04
Johann Verghese <johann.verghese@gmail.com>
2013-08-08 23:47:50 -0700

    Fix bug in build script.

--- build.sh
+++ build.sh
@@ -3,24 +3,25 @@
 # $3: String to replace the line.
 function replaceLine() {
   # Print out lines till lineno argument.
-  echo "$2" | head -n `expr $1 - 1`
+  echo "$2" | head -n `expr $1 - 1` >> rax
   totallines=$(echo "$2" | wc -l)
   # Print out dependency lib.
-  echo "\n## INLINING ##\n"
-  echo "$3"
+  echo "\n## INLINING ##\n" >> rax
+  cat "$3" >> rax
   # Print out rest of file.
-  echo "$2" | tail -n `expr $totallines - $1`
+  echo "$2" | tail -n `expr $totallines - $1` >> rax
 }
 
+rm -f rax
+touch rax
 # Inline sourced dependencies.
 source=$(cat src/rax)
 deps=$(echo "$source" | grep '. $libdir')
 while read line; do
   lineno=$(echo "$source" | grep -n "$line" |cut -f1 -d:)
-  libsrc=$(echo $line | sed 's/\. \$libdir/lib/' | xargs cat)
+  libsrc=$(echo $line | sed 's/\. \$libdir/lib/')
   source=$(replaceLine $lineno "$source" "$libsrc")
 done <<< "$deps"
 
-rm -f rax
 echo "$source" >> rax
 chmod +x rax
